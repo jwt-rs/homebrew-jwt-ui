945e0d671e6e78abcd7b0b847f8f8639a3e7e894
Deepu <d4udts@gmail.com>
2024-01-10 15:25:29 +0100

    brew bucket config

--- Formula/jwt-ui.rb
+++ Formula/jwt-ui.rb
@@ -1,6 +1,6 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
-class jwtui < Formula
+class Jwtui < Formula
   desc "A Terminal UI for decoding/encoding JSON Web Tokens"
   homepage "https://github.com/jwt-rs/jwt-ui"
   if OS.mac?
