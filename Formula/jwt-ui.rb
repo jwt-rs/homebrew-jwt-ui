4448f63ed0699255b98d3a9dbb0047ba3857cf81
deepu105 <d4udts@gmail.com>
2024-01-11 15:13:34 +0000

    Update formula for jwt-ui release v0.0.4

--- Formula/jwt-ui.rb
+++ Formula/jwt-ui.rb
@@ -1,16 +1,17 @@
+
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 class JwtUi < Formula
   desc "A Terminal UI for decoding/encoding JSON Web Tokens"
   homepage "https://github.com/jwt-rs/jwt-ui"
   if OS.mac?
-    url "https://github.com/jwt-rs/jwt-ui/releases/download/0.0.1/jwtui-macos.tar.gz"
-    sha256 "$hash_mac"
+    url "https://github.com/jwt-rs/jwt-ui/releases/download/v0.0.4/jwtui-macos.tar.gz"
+    sha256 "1fc881f8cf265bc4ac84af7a14484bf6a6dc658d72e127f0e85a9bbdb356bd8b"
   else
-    url "https://github.com/jwt-rs/jwt-ui/releases/download/0.0.1/jwtui-linux.tar.gz"
-    sha256 "$hash_linux"
+    url "https://github.com/jwt-rs/jwt-ui/releases/download/v0.0.4/jwtui-linux.tar.gz"
+    sha256 "4337dcaef391e953cee021fd60b07929635b77c11667f91c1c078cfcb2f2e0d1"
   end
-  version "0.0.1"
+  version "v0.0.4"
   license "MIT"
 
   def install
